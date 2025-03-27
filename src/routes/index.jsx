d818adfe8c51966f026e4ccf28cf8dd108ad8a6f
Maikel-rich <miguel.ricofer2005@gmail.com>
2025-03-27 13:58:53 +0100

    ♻️ Implement routing

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,24 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "../pages/Landing";
+import Landing from "../pages/Landing.jsx";
 import Login from "../pages/Login.jsx";
 import DashboardLayout from "../layouts/DashboardLayout";
 import DashboardHome from "../pages/Dashboard/DashboardHome";
 import Products from "../pages/Dashboard/DashboardProducts.jsx";
-import ViewList from "../pages/Dashboard/DashnboardViewLists.jsx";
+import ViewList from "../pages/Dashboard/DashboardViewLists.jsx";
 
 const AppRoutes = () => {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/dashboard" element={<DashboardLayout />}>
-                    <Route index element={<DashboardHome />} />
-                    <Route path="products" element={<Products />} />
-                    <Route path="lists" element={<ViewList />} />
-                </Route>
-            </Routes>
-        </Router>
+        <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<DashboardLayout />}>
+                <Route index element={<DashboardHome />} />
+                <Route path="products" element={<Products />} />
+                <Route path="lists" element={<ViewList />} />
+            </Route>
+        </Routes>
     );
 };
 
