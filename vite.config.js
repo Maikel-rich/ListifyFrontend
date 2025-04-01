b781de7f6e476f47f4a75ce84a69140004d7fdba
Maikel-rich <miguel.ricofer2005@gmail.com>
2025-04-01 20:43:44 +0200

    🔧 Add tsconfig

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
 
-// https://vite.dev/config/
+// Define __dirname usando ES modules
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss()
   ],
   base: "./",
-})
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      { find: '@models', replacement: path.resolve(__dirname, 'src/models') },
+      { find: '@services', replacement: path.resolve(__dirname, 'src/services') }
+    ]
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
+});
